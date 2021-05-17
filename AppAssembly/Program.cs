0f8e9f930d950ab09864df4823791c866681cf8f
Lucky <Lucky@LUCKY-PC>
2021-05-17 16:08:41 +0300

    Add project files.

---
+++ AppAssembly/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using NixDll_1;
+
+namespace AppAssembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type myType1 = typeof(User1);
+
+            //Type myType2 = Type.GetType(myType1.ToString(), false, true);
+
+            foreach (MemberInfo mi in myType1.GetMembers())
+            {
+                Console.WriteLine($"{mi.DeclaringType} {mi.MemberType} {mi.Name}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
